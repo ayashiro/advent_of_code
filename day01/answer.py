a347680b86b2dc098c9274a58c6584aa248bc5fb
Kenta Utsunomiya <kentau@google.com>
2022-12-01 10:48:33 +0000

    update the solution to use read() function instead of readlines for avoiding duplicated logic

--- day01/answer.py
+++ day01/answer.py
@@ -1,1 +1,2 @@
-print(max( sum(tuple(map(int,filter(lambda s :len(s) > 0 ,v.split("\n"))))) for v in "".join( open("input").readlines()).split("\n\n")))
+with open("input") as f :
+    print(max(sum(map(int, v.split())) for v in f.read().strip().split("\n\n")))

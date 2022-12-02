a5f7e2b66cefcba2e01cc1c01f56fbffc7528a09
Kenta Utsunomiya <kentau@google.com>
2022-12-02 05:39:02 +0000

    make answer1 one liner

--- day02/answer.py
+++ day02/answer.py
@@ -1,15 +1,2 @@
 import sys
-with open (sys.argv[1]) as f:
-    point_for_shape = [1,2,3]
-    point_for_result = [0,3,6]
-    convert = {"X" : 0 , "Y": 1, "Z" : 2, "A":0, "B":1, "C":2}
-    ans = 0
-    for a,b in [line.strip().split() for line in f.readlines()]:
-        print(a,b)
-        player = convert[b]
-        computer = convert[a]
-        s_p = point_for_shape[player]
-        result = point_for_result[(( player - computer) + 1)%3]
-        print(f"{s_p} + {result}")
-        ans += s_p + result
-    print(ans)
+with open (sys.argv[1]) as u:print(sum((lambda p,c:[1,2,3][p] + [0,3,6][((p-c) + 1) %3])(*map(lambda t:{"X" : 0 , "Y": 1, "Z" : 2, "A":0, "B":1, "C":2}[t],(b,a))) for a,b in [l.strip().split() for l in u.readlines()]))

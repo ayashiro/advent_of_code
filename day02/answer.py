75eb6b78e405ab04ac853cd8976d8289dde5582a
Kenta Utsunomiya <kentau@google.com>
2022-12-02 09:03:40 +0000

    modity the short coding logic for the forst part

--- day02/answer.py
+++ day02/answer.py
@@ -1,2 +1,3 @@
 import sys
-with open (sys.argv[1]) as u:print(sum((lambda p,c:[1,2,3][p] + [0,3,6][((p-c) + 1) %3])(*map(lambda t:{"X" : 0 , "Y": 1, "Z" : 2, "A":0, "B":1, "C":2}[t],(b,a))) for a,b in [l.strip().split() for l in u.readlines()]))
+with open (sys.argv[1]) as u:
+    print(sum(p + 1 + 3*((p-c + 1) %3) for c,p in (map(lambda t:'ABCXYZ'.index(t)%3,l.strip().split()) for l in u.readlines())))
